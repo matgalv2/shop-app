17149beecb18e3e01e6d5284c868bc20332524fe
mateusz <matgalv2@gmail.com>
2024-09-28 00:15:28 +0200

    Updated

--- modules/client/infrastructure/src/main/scala/io/github/g4lowy/client/infrastructure/repository/ClientRepositoryPostgres.scala
+++ modules/client/infrastructure/src/main/scala/io/github/g4lowy/client/infrastructure/repository/ClientRepositoryPostgres.scala
@@ -1,98 +1,94 @@
-//package io.github.g4lowy.client.infrastructure.repository
-//
-//
-//import io.getquill
-//import io.getquill.{CamelCase, PostgresJdbcContext}
-//import io.github.g4lowy.client.domain.model.{Client, ClientError, ClientId}
-//import io.github.g4lowy.client.domain.repository.ClientRepository
-//import io.github.g4lowy.client.infrastructure.model.ClientSQL
-//import zio.{IO, UIO, ZIO}
-//
-//case class ClientRepositoryPostgres(ctx: PostgresJdbcContext[CamelCase]) extends ClientRepository{
-//
-//  import ctx._
-//
-//  private val clients = quote {
-//    querySchema[ClientSQL]("Clients")
-//  }
-//
-//  override def create(client: Client): UIO[ClientId] = {
-//    val result =
-//      run{
-//        quote {
-//          clients.insertValue(lift(ClientSQL.fromDomain(client)))
-//        }
-//      }
-//
-//    result
-//      .map(_ => client.clientId)
-//      .orDieWith(x => x)
-//  }
-//
-//  override def getById(clientId: ClientId): IO[ClientError.NotFound, Client] = {
-//    val result =
-//      run {
-//        quote{
-//          clients.filter(_.clientId == lift(clientId.value))
-//        }
-//      }
-//      result
-//      .map(_.headOption.map(_.toDomain()))
-//      .some
-//      .orElseFail(ClientError.NotFound(clientId))
-//  }
-//
-//  override def getAll: UIO[List[Client]] = {
-//    val result = run(quote(clients))
-//
-//    result
-//      .map(_.map(_.toDomain()))
-//      .orDieWith(x => x)
-//  }
-//
-//  override def update(clientId: ClientId, client: Client): IO[ClientError.NotFound, Unit] = {
-//    val result = run{
-//      quote{
-//        clients
-//          .filter(_.clientId == lift(clientId.value))
-//          .update(
-//            _.firstName -> lift(client.firstName.value),
-//            _.lastName -> lift(client.lastName.value),
-//            _.phone -> lift(client.phone.value)
-//          )
-//      }
-//    }
-//
-//    result
-//      .orDieWith(x => x)
-//      .flatMap( rowsNo =>
-//        if (rowsNo == 1) ZIO.unit
-//        else  ZIO.fail(ClientError.NotFound(clientId))
-//      )
-//  }
-//
-//  override def delete(clientId: ClientId): UIO[Unit] = {
-//    val result = run{
-//      quote{
-//        clients
-//          .filter(_.clientId == lift(clientId.value))
-//          .delete
-//      }
-//    }
-//
-//    result
-//      .flatMap( rowsNo =>
-//        if (rowsNo == 1) ZIO.unit
-//        else  ZIO.fail(ClientError.NotFound(clientId))
-//      )
-//      .orDieWith(x => new Exception(""))
-//  }
-//
-//
-//
-//
-//}
-//
-//object ClientRepositoryPostgres {
-//
-//}
+package io.github.g4lowy.client.infrastructure.repository
+
+import io.getquill.CamelCase
+import io.getquill.jdbczio.Quill
+import io.github.g4lowy.client.domain.model.{ Client, ClientError, ClientId }
+import io.github.g4lowy.client.domain.repository.ClientRepository
+import io.github.g4lowy.client.infrastructure.model.ClientSQL
+import io.github.g4lowy.error.DatabaseCriticalFailure
+import zio.{ IO, UIO, ZIO, ZLayer }
+
+case class ClientRepositoryPostgres(quill: Quill.Postgres[CamelCase]) extends ClientRepository {
+
+  import quill._
+
+  private val clients = quote {
+    querySchema[ClientSQL]("Clients")
+  }
+
+  override def create(client: Client): UIO[ClientId] = {
+    val result =
+      run {
+        quote {
+          clients.insertValue(lift(ClientSQL.fromDomain(client)))
+        }
+      }
+
+    result
+      .as(client.clientId)
+      .orDieWith(error => DatabaseCriticalFailure(error.getMessage))
+  }
+
+  override def getById(clientId: ClientId): IO[ClientError.NotFound, Client] = {
+    val result =
+      run {
+        quote {
+          clients.filter(_.clientId == lift(clientId.value))
+        }
+      }
+    result
+      .orDieWith(error => DatabaseCriticalFailure(error.getMessage))
+      .flatMap(_.headOption match {
+        case Some(value) => ZIO.succeed(value.toDomain())
+        case None        => ZIO.fail(ClientError.NotFound(clientId))
+      })
+  }
+
+  override def getAll: UIO[List[Client]] = {
+    val result = run(quote(clients))
+
+    result
+      .map(_.map(_.toDomain()))
+      .orDieWith(fail => DatabaseCriticalFailure(fail.getMessage))
+  }
+
+  override def update(clientId: ClientId, client: Client): IO[ClientError.NotFound, Unit] = {
+    val result = run {
+      quote {
+        clients
+          .filter(_.clientId == lift(clientId.value))
+          .update(
+            _.firstName -> lift(client.firstName.value),
+            _.lastName -> lift(client.lastName.value),
+            _.phone -> lift(client.phone.value)
+          )
+      }
+    }
+
+    result
+      .orDieWith(error => DatabaseCriticalFailure(error.getMessage))
+      .flatMap(rowsNo => ZIO.unless(rowsNo == 1)(ZIO.fail(ClientError.NotFound(clientId))))
+      .unit
+  }
+
+  override def delete(clientId: ClientId): IO[ClientError.NotFound, Unit] = {
+    val result = run {
+      quote {
+        clients
+          .filter(_.clientId == lift(clientId.value))
+          .delete
+      }
+    }
+
+    result
+      .orDieWith(error => DatabaseCriticalFailure(error.getMessage))
+      .flatMap(rowsNo => ZIO.fail(ClientError.NotFound(clientId)).unless(rowsNo == 1))
+      .unit
+  }
+
+}
+
+object ClientRepositoryPostgres {
+  val live: ZLayer[Quill.Postgres[CamelCase], Nothing, ClientRepositoryPostgres] =
+    ZLayer.fromFunction(ClientRepositoryPostgres.apply _)
+}
