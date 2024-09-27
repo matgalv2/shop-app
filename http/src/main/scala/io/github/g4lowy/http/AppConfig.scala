2ab49f93426eec4a633cdd261dc0acfb1a60c516
mateusz <matgalv2@gmail.com>
2024-09-27 23:19:12 +0200

    Added application config descriptors

---
+++ http/src/main/scala/io/github/g4lowy/http/AppConfig.scala
@@ -0,0 +1,40 @@
+package io.github.g4lowy.http
+
+import com.comcast.ip4s.{Host, Port}
+import zio.ZLayer
+import zio.config._
+import zio.config.magnolia.descriptor
+import zio.config.typesafe.{TypesafeConfig, TypesafeConfigSource}
+
+final case class AppConfig(http: AppConfig.Http, database: AppConfig.Database)
+
+object AppConfig {
+
+  private val appConfigDescriptor: ConfigDescriptor[AppConfig] =
+    (ConfigDescriptor.nested("http")(httpDesc) <*> ConfigDescriptor.nested("database")(databaseDesc)) map {
+      case (http, db) =>
+        AppConfig(http, db)
+    }
+
+  val configLive = TypesafeConfig.fromResourcePath(appConfigDescriptor).orDie
+
+  final case class Http(port: Port, host: Host)
+
+  object Http {
+    val hostDescriptor: ConfigDescriptor[Host] =
+      ConfigDescriptor.string.transformOrFailLeft(Host.fromString(_).toRight("Invalid data"))(_.toString)
+
+    val portDescriptor: ConfigDescriptor[Port] =
+      ConfigDescriptor.int.transformOrFailLeft(Port.fromInt(_).toRight("invalid data"))(_.value)
+  }
+
+  val httpDesc: ConfigDescriptor[Http] =
+    (ConfigDescriptor.nested("port")(Http.portDescriptor) zip ConfigDescriptor.nested("host")(
+      Http.hostDescriptor
+    )).map { case (port, host) => Http(port, host) }
+
+  final case class Database(url: String, name: String, schema: String, username: String, password: String)
+
+  val databaseDesc = descriptor[Database]
+
+}
