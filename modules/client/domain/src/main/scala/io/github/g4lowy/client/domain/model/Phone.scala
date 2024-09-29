2de0b0f364959d364e24db7abc76b0475fd0f913
mateusz <matgalv2@gmail.com>
2024-09-29 21:22:33 +0200

    Updated pattern

--- modules/client/domain/src/main/scala/io/github/g4lowy/client/domain/model/Phone.scala
+++ modules/client/domain/src/main/scala/io/github/g4lowy/client/domain/model/Phone.scala
@@ -8,7 +8,7 @@ case class Phone private (value: String)
 object Phone {
   final case class Unvalidated(number: String) extends NotValidated[Phone] {
     override def validate: Validation[FailureDescription, Phone] =
-      matchesRegex("^\\+[0-9]{1,3} [0-9]{6,}$".r).apply(number).map(Phone.apply)
+      matchesRegex("^\\+[0-9]{1,3}-[0-9]{6,}$".r).apply(number).map(Phone.apply)
 
     override def unsafeValidation: Phone = Phone.apply(number)
   }
