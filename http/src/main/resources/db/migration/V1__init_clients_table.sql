32ee72d80b65222326268369f0dcda74f06f60a4
mateusz <matgalv2@gmail.com>
2024-10-01 20:35:35 +0200

    Added birthDate field to Clients table

--- http/src/main/resources/db/migration/V1__init_clients_table.sql
+++ http/src/main/resources/db/migration/V1__init_clients_table.sql
@@ -7,6 +7,6 @@ CREATE TABLE Clients (
     createdAt   TIMESTAMP   NOT NULL
 );
 
-ALTER TABLE Clients ADD CONSTRAINT firstName_check CHECK (firstName ~* '^[A-Z][a-z]{0,31}$');
-ALTER TABLE Clients ADD CONSTRAINT lastName_check CHECK (lastName ~* '^[A-Z][a-z]{0,31}$');
+ALTER TABLE Clients ADD CONSTRAINT firstName_check CHECK (firstName ~* '^[A-Z]([A-z]|[''` -]){0,31}$');
+ALTER TABLE Clients ADD CONSTRAINT lastName_check CHECK (lastName ~* '^[A-Z]([A-z]|[''` -]){0,31}$');
 ALTER TABLE Clients ADD CONSTRAINT phone_check CHECK (phone ~* '^\+[0-9]{1,3}-[0-9]{6,}$');
