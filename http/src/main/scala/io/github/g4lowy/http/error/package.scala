7b97c2b66042e31ca35f22390e97c60afaa3da34
mateusz <matgalv2@gmail.com>
2024-10-01 20:38:29 +0200

    Added ErrorMessage for validation failure description

--- http/src/main/scala/io/github/g4lowy/http/error/package.scala
+++ http/src/main/scala/io/github/g4lowy/http/error/package.scala
@@ -2,8 +2,12 @@ package io.github.g4lowy.http
 
 import io.github.g4lowy.client.domain.model.ClientError
 import io.github.g4lowy.error.ErrorMessage
+import io.github.g4lowy.validation.validators.Validator
 
 package object error {
+
+  implicit val validation: ErrorMessage[Validator.FailureDescription] = description =>
+    s"Validation failure description: $description"
 
   implicit val clientNotFound: ErrorMessage[ClientError.NotFound] = { case ClientError.NotFound(clientId) =>
     s"Client with id: $clientId was not found"
