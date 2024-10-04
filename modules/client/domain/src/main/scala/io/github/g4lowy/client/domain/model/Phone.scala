3303ba02951da4a35c08fbb2007a39c90ac354af
mateusz <matgalv2@gmail.com>
2024-10-04 14:02:22 +0200

    Updated

--- modules/client/domain/src/main/scala/io/github/g4lowy/client/domain/model/Phone.scala
+++ modules/client/domain/src/main/scala/io/github/g4lowy/client/domain/model/Phone.scala
@@ -6,10 +6,10 @@ import io.github.g4lowy.validation.validators.Validator.{ matchesRegex, FailureD
 case class Phone private (value: String)
 
 object Phone {
-  final case class Unvalidated(number: String) extends NotValidated[Phone] {
+  final case class Unvalidated(value: String) extends NotValidated[Phone] {
     override def validate: Validation[FailureDescription, Phone] =
-      matchesRegex("^\\+[0-9]{1,3}-[0-9]{6,}$".r).apply(number).map(Phone.apply)
+      matchesRegex("^\\+[0-9]{1,3}-[0-9]{6,}$".r).apply(value).map(Phone.apply)
 
-    override def unsafeValidation: Phone = Phone.apply(number)
+    override def unsafeValidation: Phone = Phone.apply(value)
   }
 }
