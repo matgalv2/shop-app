99ce428ce6058e20e1382466bb012c0a6b183821
mateusz <matgalv2@gmail.com>
2024-10-08 18:24:06 +0200

    Added error message for ProductError

--- http/src/main/scala/io/github/g4lowy/http/error/package.scala
+++ http/src/main/scala/io/github/g4lowy/http/error/package.scala
@@ -1,6 +1,7 @@
 package io.github.g4lowy.http
 
 import io.github.g4lowy.client.domain.model.ClientError
+import io.github.g4lowy.product.domain.model.ProductError
 import io.github.g4lowy.error.ErrorMessage
 import io.github.g4lowy.validation.validators.Validator
 
@@ -11,5 +12,9 @@ package object error {
 
   implicit val clientNotFound: ErrorMessage[ClientError.NotFound] = { case ClientError.NotFound(clientId) =>
     s"Client with id: $clientId was not found"
+  }
+
+  implicit val productNotFound: ErrorMessage[ProductError.NotFound] = { case ProductError.NotFound(productId) =>
+    s"Product with id: $productId was not found"
   }
 }
