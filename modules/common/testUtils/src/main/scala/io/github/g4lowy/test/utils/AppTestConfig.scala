b609bd6a1c863ba00e95a7c3ebabe546ab9ebf1b
mateusz <matgalv2@gmail.com>
2024-10-09 18:15:55 +0200

    Added config for acceptance tests

--- modules/common/testUtils/src/main/scala/io/github/g4lowy/test/utils/AppTestConfig.scala
+++ modules/common/testUtils/src/main/scala/io/github/g4lowy/test/utils/AppTestConfig.scala
@@ -9,15 +9,21 @@ final case class AppTestConfig(database: AppTestConfig.Database)
 
 object AppTestConfig {
 
+  private val integrationTestApplicationConfigFilePath =
+    "modules/common/testUtils/src/main/resources/application-integration_test.conf"
+  private val acceptanceTestApplicationConfigFilePath =
+    "modules/common/testUtils/src/main/resources/application-acceptance_test.conf"
+
   private val testConfigDescriptor: ConfigDescriptor[AppTestConfig] =
     ConfigDescriptor.nested("database")(databaseDesc).to[AppTestConfig]
 
-  def testConfigLive(): ULayer[AppTestConfig] = TypesafeConfig
-    .fromHoconFilePath(
-      filePath = "modules/common/testUtils/src/main/resources/application-test.conf",
-      testConfigDescriptor
-    )
+  private def testConfigLive(hoconFilePath: String): ULayer[AppTestConfig] = TypesafeConfig
+    .fromHoconFilePath(filePath = hoconFilePath, testConfigDescriptor)
     .orDie
+
+  def integrationTestConfigLive: ULayer[AppTestConfig] = testConfigLive(integrationTestApplicationConfigFilePath)
+
+  def acceptanceTestConfigLive: ULayer[AppTestConfig] = testConfigLive(acceptanceTestApplicationConfigFilePath)
 
   final case class Database(url: String, name: String, schema: String, username: String, password: String)
 
---
+++ modules/common/testUtils/src/main/scala/io/github/g4lowy/test/utils/PostgresIntegrationTestDatabaseMigrationRunner.scala
@@ -0,0 +1,11 @@
+package io.github.g4lowy.test.utils
+
+import io.github.g4lowy.test.utils.TestDatabaseConfiguration.{ dataSource, flyway }
+import zio.{ Scope, ZIO, ZIOAppArgs, ZIOAppDefault }
+
+object PostgresIntegrationTestDatabaseMigrationRunner extends ZIOAppDefault {
+
+  override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] =
+    flyway.provide(AppTestConfig.integrationTestConfigLive, dataSource)
+
+}
