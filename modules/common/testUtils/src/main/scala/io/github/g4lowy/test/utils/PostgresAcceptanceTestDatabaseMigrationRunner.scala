ddcace91a4ebcef0ef9f613c61975aae26e1bc30
mateusz <matgalv2@gmail.com>
2024-10-11 00:01:20 +0200

    Migration runner for acceptance tests database

--- modules/common/testUtils/src/main/scala/io/github/g4lowy/test/utils/PostgresAcceptanceTestDatabaseMigrationRunner.scala
+++ modules/common/testUtils/src/main/scala/io/github/g4lowy/test/utils/PostgresAcceptanceTestDatabaseMigrationRunner.scala
@@ -1,11 +1,11 @@
 package io.github.g4lowy.test.utils
 
-import io.github.g4lowy.test.utils.TestDatabaseConfiguration.{ dataSource, flyway }
+import io.github.g4lowy.test.utils.TestDatabaseConfiguration.{ quillDataSource, flyway }
 import zio.{ Scope, ZIO, ZIOAppArgs, ZIOAppDefault }
 
 object PostgresAcceptanceTestDatabaseMigrationRunner extends ZIOAppDefault {
 
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] =
-    flyway.provide(AppTestConfig.acceptanceTestConfigLive, dataSource)
+    flyway.provide(AppTestConfig.acceptanceTestConfigLive, quillDataSource)
 
 }
