f17761a43b9feea7254ef578cdb23ee85053e76b
mateusz <matgalv2@gmail.com>
2024-10-27 14:32:48 +0100

    Added order status and customerId

--- http/src/main/resources/db/migration/V4__init_orders_table.sql
+++ http/src/main/resources/db/migration/V4__init_orders_table.sql
@@ -1,15 +1,19 @@
 CREATE TYPE PAYMENT_TYPE AS ENUM ('BANK_TRANSFER', 'CARD', 'ON_DELIVERY');
 CREATE TYPE SHIPMENT_TYPE AS ENUM ('COURIER', 'BOX', 'ON_PLACE');
+CREATE TYPE ORDER_STATUS AS ENUM ('CREATED', 'CANCELLED', 'PAID', 'IN_PROGRESS', 'SENT', 'DELIVERED');
 
 CREATE TABLE Orders(
-    orderId UUID,
-    paymentType PAYMENT_TYPE    NOT NULL,
-    paymentAddressId UUID       NOT NULL,
-    shipmentType SHIPMENT_TYPE  NOT NULL,
-    shipmentAddressId UUID,
+    orderId             UUID,
+    customerId          UUID            NOT NULL,
+    paymentType         PAYMENT_TYPE    NOT NULL,
+    paymentAddressId    UUID            NOT NULL,
+    shipmentType        SHIPMENT_TYPE   NOT NULL,
+    shipmentAddressId   UUID,
+    status              ORDER_STATUS    NOT NULL,
 
     PRIMARY KEY (orderId),
-    FOREIGN KEY (shipmentAddressId) REFERENCES Addresses (addressId)
+    FOREIGN KEY (shipmentAddressId) REFERENCES Addresses (addressId),
+    FOREIGN KEY (customerId) REFERENCES Customers (customerId)
 );
 
 ALTER TABLE Orders ADD CONSTRAINT shipment_check CHECK (
