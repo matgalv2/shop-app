da1e265230d40f6ee45ef56b4dc12589cabb2975
mateusz <matgalv2@gmail.com>
2024-11-09 17:15:57 +0100

    Moved Address to order package

--- modules/order/domain/src/main/scala/io/github/g4lowy/order/domain/model/Address.scala
+++ modules/order/domain/src/main/scala/io/github/g4lowy/order/domain/model/Address.scala
@@ -1,55 +1,83 @@
-package io.github.g4lowy.client.domain.model
+package io.github.g4lowy.order.domain.model
 
-import io.github.g4lowy.validation.validators.Validation
+import io.github.g4lowy.validation.validators.{ NotValidated, Validation }
 import Address._
-import io.github.g4lowy.validation.extras.NotValidated
 import io.github.g4lowy.validation.validators.Validator.FailureDescription
-
 import io.github.g4lowy.validation.validators.Validator._
 
-final case class Address private(country: Country, city: City, street: Street, zipCode:ZipCode, building: Building, apartment: Option[Apartment])
+import java.util.UUID
 
-object Address{
+final case class Address private (
+  addressId: AddressId,
+  country: Country,
+  city: City,
+  street: Street,
+  zipCode: ZipCode,
+  building: Building,
+  apartment: Option[Apartment]
+)
 
-  final case class Unvalidated(country: Country.Unvalidated,
-                               city: City.Unvalidated,
-                               street: Street.Unvalidated,
-                               zipCode: ZipCode.Unvalidated,
-                               building: Building.Unvalidated,
-                               apartment: Option[Apartment.Unvalidated]
-                              ) extends NotValidated[Address] {
+object Address {
+
+  final case class Unvalidated(
+    addressId: AddressId.Unvalidated,
+    country: Country.Unvalidated,
+    city: City.Unvalidated,
+    street: Street.Unvalidated,
+    zipCode: ZipCode.Unvalidated,
+    building: Building.Unvalidated,
+    apartment: Option[Apartment.Unvalidated]
+  ) extends NotValidated[Address] {
 
     override def validate: Validation[FailureDescription, Address] =
       for {
-        countryV <- country.validate
-        cityV <- city.validate
-        streetV <- street.validate
-        zipCodeV <- zipCode.validate
-        buildingV <- building.validate
+        id         <- addressId.validate
+        countryV   <- country.validate
+        cityV      <- city.validate
+        streetV    <- street.validate
+        zipCodeV   <- zipCode.validate
+        buildingV  <- building.validate
         apartmentV <- validOrCheck[Apartment, Apartment.Unvalidated](apartment)
       } yield Address(
-        country = countryV,
-        city = cityV,
-        street = streetV,
-        zipCode = zipCodeV,
-        building = buildingV,
+        addressId = id,
+        country   = countryV,
+        city      = cityV,
+        street    = streetV,
+        zipCode   = zipCodeV,
+        building  = buildingV,
         apartment = apartmentV
       )
 
     override def unsafeValidation: Address =
       Address(
-        country = country.unsafeValidation,
-        city = city.unsafeValidation,
-        street = street.unsafeValidation,
-        zipCode = zipCode.unsafeValidation,
-        building = building.unsafeValidation,
+        addressId = addressId.unsafeValidation,
+        country   = country.unsafeValidation,
+        city      = city.unsafeValidation,
+        street    = street.unsafeValidation,
+        zipCode   = zipCode.unsafeValidation,
+        building  = building.unsafeValidation,
         apartment = apartment.map(_.unsafeValidation)
       )
 
   }
 
-  final case class Country private(value: String)
-  object Country{
+  final case class AddressId private (value: UUID)
+
+  object AddressId {
+    def generate: AddressId.Unvalidated = AddressId.Unvalidated(UUID.randomUUID.toString)
+
+    def fromUUID(uuid: UUID): AddressId = AddressId(uuid)
+
+    final case class Unvalidated(value: String) extends NotValidated[AddressId] {
+      def validate: Validation[FailureDescription, AddressId] =
+        uuid.apply(value).map(uuid => AddressId.apply(UUID.fromString(uuid)))
+
+      def unsafeValidation: AddressId = AddressId(UUID.fromString(value))
+    }
+  }
+
+  final case class Country private (value: String)
+  object Country {
     final case class Unvalidated(value: String) extends NotValidated[Country] {
       override def validate: Validation[FailureDescription, Country] =
         (nonEmpty and nonBlank and capitalized).apply(value).map(Country.apply)
@@ -58,8 +86,8 @@ object Address{
     }
   }
 
-  final case class City private(value: String)
-  object City{
+  final case class City private (value: String)
+  object City {
     final case class Unvalidated(value: String) extends NotValidated[City] {
       override def validate: Validation[FailureDescription, City] =
         (nonEmpty and nonBlank and capitalized).apply(value).map(City.apply)
@@ -68,8 +96,8 @@ object Address{
     }
   }
 
-  final case class Street private(value: String)
-  object Street{
+  final case class Street private (value: String)
+  object Street {
     final case class Unvalidated(value: String) extends NotValidated[Street] {
       override def validate: Validation[FailureDescription, Street] =
         (nonEmpty and nonBlank and capitalized).apply(value).map(Street.apply)
@@ -78,8 +106,8 @@ object Address{
     }
   }
 
-  final case class ZipCode private(value: String)
-  object ZipCode{
+  final case class ZipCode private (value: String)
+  object ZipCode {
     final case class Unvalidated(value: String) extends NotValidated[ZipCode] {
       override def validate: Validation[FailureDescription, ZipCode] =
         matchesRegex("^[0-9]{2}-[0-9]{3}$".r).apply(value).map(ZipCode.apply)
@@ -88,9 +116,8 @@ object Address{
     }
   }
 
-
-  final case class Building private(value: String)
-  object Building{
+  final case class Building private (value: String)
+  object Building {
     final case class Unvalidated(value: String) extends NotValidated[Building] {
       override def validate: Validation[FailureDescription, Building] =
         matchesRegex("^[0-9]*[A-z]*$".r).apply(value).map(Building.apply)
@@ -99,8 +126,8 @@ object Address{
     }
   }
 
-  final case class Apartment private(value: String)
-  object Apartment{
+  final case class Apartment private (value: String)
+  object Apartment {
     final case class Unvalidated(value: String) extends NotValidated[Apartment] {
       override def validate: Validation[FailureDescription, Apartment] =
         matchesRegex("^[0-9]*[A-z]*$".r).apply(value).map(Apartment.apply)
---
+++ modules/order/infrastructure/src/main/scala/io/github/g4lowy/order/infrastructure/model/AddressSQL.scala
@@ -0,0 +1,41 @@
+package io.github.g4lowy.order.infrastructure.model
+
+import io.github.g4lowy.order.domain.model.Address
+import io.github.g4lowy.order.domain.model.Address._
+
+import java.util.UUID
+
+final case class AddressSQL(
+  addressId: UUID,
+  country: String,
+  city: String,
+  street: String,
+  zipCode: String,
+  building: String,
+  apartment: Option[String]
+) {
+
+  def toUnvalidated: Address.Unvalidated =
+    Address.Unvalidated(
+      addressId = AddressId.Unvalidated(addressId.toString),
+      country   = Country.Unvalidated(country),
+      city      = City.Unvalidated(city),
+      street    = Street.Unvalidated(street),
+      zipCode   = ZipCode.Unvalidated(zipCode),
+      building  = Building.Unvalidated(building),
+      apartment = apartment.map(Apartment.Unvalidated.apply)
+    )
+}
+
+object AddressSQL {
+  def fromDomain(address: Address): AddressSQL =
+    AddressSQL(
+      addressId = address.addressId.value,
+      country   = address.country.value,
+      city      = address.city.value,
+      street    = address.street.value,
+      zipCode   = address.zipCode.value,
+      building  = address.building.value,
+      apartment = address.apartment.map(_.value)
+    )
+}
