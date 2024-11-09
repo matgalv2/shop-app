116606fd4e30e76496f9b79d8c8247f9db1862b7
mateusz <matgalv2@gmail.com>
2024-11-09 17:14:50 +0100

    Updated

--- modules/common/testUtils/src/main/scala/io/github/g4lowy/test/utils/enums/EnumDecoder.scala
+++ modules/common/testUtils/src/main/scala/io/github/g4lowy/test/utils/enums/EnumDecoder.scala
@@ -2,7 +2,7 @@ package io.github.g4lowy.test.utils.enums
 
 trait EnumDecoder[T <: EnumSQL] {
 
-  val values: List[T]
+  protected val values: List[T]
 
   def decode(value: String): T = {
     val result = values.find(_.value == value)
