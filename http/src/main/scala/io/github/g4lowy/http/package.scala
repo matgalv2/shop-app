e1fe8ada7e692dd8a61a52995cb03fa5380e5932
mateusz <matgalv2@gmail.com>
2024-11-11 09:38:29 +0100

    Extended AppEnvironment type with OrderApi.Environment

--- http/src/main/scala/io/github/g4lowy/http/package.scala
+++ http/src/main/scala/io/github/g4lowy/http/package.scala
@@ -1,10 +1,10 @@
 package io.github.g4lowy
 
-import io.github.g4lowy.http.api.{ CustomerApi, ProductApi }
+import io.github.g4lowy.http.api.{ CustomerApi, OrderApi, ProductApi }
 import zio.&
 
 package object http {
 
-  type AppEnvironment = CustomerApi.Environment & ProductApi.Environment
+  type AppEnvironment = CustomerApi.Environment & ProductApi.Environment & OrderApi.Environment
 
 }
