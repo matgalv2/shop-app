1b728463c2d98977f9497649cfcaea80b5a35636
mateusz <matgalv2@gmail.com>
2024-11-11 16:43:49 +0100

    Moved validation and mapping dto <-> domain to service

--- http/src/main/scala/io/github/g4lowy/http/service/ProductService.scala
+++ http/src/main/scala/io/github/g4lowy/http/service/ProductService.scala
@@ -1,21 +1,48 @@
 package io.github.g4lowy.http.service
 
+import http.generated.definitions.{ CreateProduct, GetProduct, UpdateProduct }
+import io.github.g4lowy.http.converters.products._
 import io.github.g4lowy.product.domain.repository.ProductRepository
 import io.github.g4lowy.product.domain.model.{ Product, ProductError, ProductId }
-import zio.{ URIO, ZIO }
+import io.github.g4lowy.union.types.Union2
+import io.github.g4lowy.validation.extras.ZIOValidationOps
+import io.github.g4lowy.validation.validators.Validator
+import zio.{ RIO, URIO, ZIO }
+
+import java.util.UUID
 
 object ProductService {
-  def getProducts: URIO[ProductRepository, List[Product]] =
+  def getProducts: URIO[ProductRepository, Vector[GetProduct]] =
     ProductRepository.getAll
+      .map(_.map(_.toAPI))
+      .map(_.toVector)
 
-  def getProductById(productId: ProductId): ZIO[ProductRepository, ProductError.NotFound, Product] =
-    ProductRepository.getById(productId)
+  def getProductById(productId: UUID): ZIO[ProductRepository, ProductError.NotFound, Product] = {
+    val domainId = ProductId.fromUUID(productId)
+    ProductRepository.getById(domainId)
+  }
 
-  def createProduct(product: Product): URIO[ProductRepository, ProductId] = ProductRepository.create(product)
+  def createProduct(createProduct: CreateProduct): ZIO[ProductRepository, Validator.FailureDescription, ProductId] =
+    ZIO
+      .fromNotValidated(createProduct.toDomain)
+      .flatMap(ProductRepository.create)
 
-  def updateProduct(productId: ProductId, product: Product): ZIO[ProductRepository, ProductError.NotFound, Unit] =
-    ProductRepository.update(productId, product)
+  def updateProduct(
+    productId: UUID,
+    updateProduct: UpdateProduct
+  ): ZIO[ProductRepository, Union2[Validator.FailureDescription, ProductError.NotFound], Unit] =
+    ZIO
+      .fromNotValidated(updateProduct.toDomain)
+      .mapError(Union2.First.apply)
+      .flatMap { product =>
+        val domainId = ProductId.fromUUID(productId)
+        ProductRepository
+          .update(domainId, product)
+          .mapError(Union2.Second.apply)
+      }
 
-  def deleteProduct(productId: ProductId): ZIO[ProductRepository, ProductError.NotFound, Unit] =
-    ProductRepository.delete(productId)
+  def deleteProduct(productId: UUID): ZIO[ProductRepository, ProductError.NotFound, Unit] = {
+    val domainId = ProductId.fromUUID(productId)
+    ProductRepository.delete(domainId)
+  }
 }
