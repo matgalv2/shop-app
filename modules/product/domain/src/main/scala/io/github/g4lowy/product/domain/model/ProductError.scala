838d70b72053148fbc485c089f5fbda03941c15d
mateusz <matgalv2@gmail.com>
2024-11-24 13:40:46 +0100

    Updated not found error to be able to take multiple ids

--- modules/product/domain/src/main/scala/io/github/g4lowy/product/domain/model/ProductError.scala
+++ modules/product/domain/src/main/scala/io/github/g4lowy/product/domain/model/ProductError.scala
@@ -2,5 +2,5 @@ package io.github.g4lowy.product.domain.model
 
 trait ProductError
 object ProductError {
-  final case class NotFound(productId: ProductId) extends ProductError
+  final case class NotFound(productId: ProductId*) extends ProductError
 }
