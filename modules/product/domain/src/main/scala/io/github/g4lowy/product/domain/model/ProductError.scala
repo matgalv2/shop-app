14c17199eca4eb0bc6d05adf71cfc870206560a4
mateusz <matgalv2@gmail.com>
2024-11-24 21:25:12 +0100

    Made ProductError sealed and changed parameters to ensure ate least one productId is specified in NotFound entity

--- modules/product/domain/src/main/scala/io/github/g4lowy/product/domain/model/ProductError.scala
+++ modules/product/domain/src/main/scala/io/github/g4lowy/product/domain/model/ProductError.scala
@@ -1,6 +1,6 @@
 package io.github.g4lowy.product.domain.model
 
-trait ProductError
+sealed trait ProductError
 object ProductError {
-  final case class NotFound(productId: ProductId*) extends ProductError
+  final case class NotFound(productId: ProductId, productIds: ProductId*) extends ProductError
 }
