ee4604348b0a586252595211cabba5cedb8b5252
mateusz <matgalv2@gmail.com>
2024-11-24 13:45:05 +0100

    Added instances for order errors

--- http/src/main/scala/io/github/g4lowy/http/error/package.scala
+++ http/src/main/scala/io/github/g4lowy/http/error/package.scala
@@ -3,18 +3,33 @@ package io.github.g4lowy.http
 import io.github.g4lowy.customer.domain.model.CustomerError
 import io.github.g4lowy.product.domain.model.ProductError
 import io.github.g4lowy.error.ErrorMessage
+import io.github.g4lowy.order.domain.model.OrderError
 import io.github.g4lowy.validation.validators.Validator
 
 package object error {
 
-  implicit val validation: ErrorMessage[Validator.FailureDescription] = description =>
+  implicit val validationError: ErrorMessage[Validator.FailureDescription] = description =>
     s"Validation failure description: $description"
 
-  implicit val customerNotFound: ErrorMessage[CustomerError.NotFound] = { case CustomerError.NotFound(clientId) =>
+  implicit val customerErrorNotFound: ErrorMessage[CustomerError.NotFound] = { case CustomerError.NotFound(clientId) =>
     s"Client with id: $clientId was not found"
   }
 
-  implicit val productNotFound: ErrorMessage[ProductError.NotFound] = { case ProductError.NotFound(productId) =>
+  implicit val productErrorNotFound: ErrorMessage[ProductError.NotFound] = { case ProductError.NotFound(productId) =>
     s"Product with id: $productId was not found"
+  }
+
+  implicit val orderError: ErrorMessage[OrderError] = {
+    case error: OrderError.NotFound      => orderErrorNotFound.message(error)
+    case error: OrderError.InvalidStatus => orderErrorInvalidStatus.message(error)
+  }
+
+  implicit val orderErrorNotFound: ErrorMessage[OrderError.NotFound] = { case OrderError.NotFound(orderId) =>
+    s"Order with id: $orderId was not found"
+  }
+
+  implicit val orderErrorInvalidStatus: ErrorMessage[OrderError.InvalidStatus] = {
+    case OrderError.InvalidStatus(orderId, status) =>
+      s"Status od an order with id: $orderId can not be set to $status"
   }
 }
