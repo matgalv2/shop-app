4dd1e3eaebd5684d96bd93107781365dbbd9cf0c
mateusz <matgalv2@gmail.com>
2024-11-24 13:55:01 +0100

    Updated error for create method

--- modules/order/domain/src/main/scala/io/github/g4lowy/order/domain/repository/OrderRepository.scala
+++ modules/order/domain/src/main/scala/io/github/g4lowy/order/domain/repository/OrderRepository.scala
@@ -1,12 +1,13 @@
 package io.github.g4lowy.order.domain.repository
 
-import io.github.g4lowy.order.domain.model.{ Order, OrderError, OrderId, OrderStatus }
-import zio.{ IO, UIO }
+import io.github.g4lowy.order.domain.model.{Order, OrderError, OrderId, OrderStatus}
+import io.github.g4lowy.product.domain.model.ProductError
+import zio.{IO, UIO}
 import zio.macros.accessible
 
 @accessible
 trait OrderRepository {
-  def create(order: Order): UIO[OrderId]
+  def create(order: Order): IO[ProductError.NotFound, OrderId]
   def getAll(offset: Int, limit: Int): UIO[List[Order]]
   def getById(orderId: OrderId): IO[OrderError.NotFound, Order]
   def updateStatus(orderId: OrderId, orderStatus: OrderStatus): IO[OrderError, Unit]
