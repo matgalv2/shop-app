9695f709af6d653afe52047e6de982d394d9438c
mateusz <matgalv2@gmail.com>
2024-11-24 14:01:25 +0100

    Updated methods to work on DTOs

--- http/src/main/scala/io/github/g4lowy/http/service/OrderService.scala
+++ http/src/main/scala/io/github/g4lowy/http/service/OrderService.scala
@@ -1,27 +1,54 @@
 package io.github.g4lowy.http.service
 
-import http.generated.definitions.CreateOrder
+import io.github.g4lowy.customer.domain.model.{ CustomerError, CustomerId }
+import io.github.g4lowy.customer.domain.repository.CustomerRepository
+import io.github.g4lowy.http.ValidationFailure
+import io.github.g4lowy.http.converters.orders.{ OrderDetailDtoOps, OrderDtoOps }
+import io.github.g4lowy.http.dto.OrderDto
 import io.github.g4lowy.order.domain.model.{ Order, OrderError, OrderId, OrderStatus }
 import io.github.g4lowy.order.domain.repository.OrderRepository
-import io.github.g4lowy.product.domain.model.{ Product, ProductError, ProductId }
+import io.github.g4lowy.product.domain.model.{ ProductError, ProductId }
 import io.github.g4lowy.product.domain.repository.ProductRepository
-import zio.{ URIO, ZIO }
+import io.github.g4lowy.union.types.Union3
+import io.github.g4lowy.validation.extras.ZIOValidationOps
+import zio.{ &, URIO, ZIO }
+
+import java.util.UUID
 
 object OrderService {
 
   private val DEFAULT_OFFSET = 0
   private val DEFAULT_LIMIT  = 26
 
-  def getOrders(offset: Int, limit: Int): URIO[OrderRepository, List[Order]] =
-    OrderRepository.getAll(offset, limit)
+  def getOrders(offset: Option[Int], limit: Option[Int]): URIO[OrderRepository, List[Order]] =
+    OrderRepository.getAll(offset.getOrElse(DEFAULT_OFFSET), limit.getOrElse(DEFAULT_LIMIT))
 
-  def getOrderById(orderId: OrderId): ZIO[OrderRepository, OrderError.NotFound, Order] =
-    OrderRepository.getById(orderId)
+  def getOrderById(orderId: UUID): ZIO[OrderRepository, OrderError.NotFound, Order] = {
+    val domainId = OrderId.fromUUID(orderId)
+    OrderRepository.getById(domainId)
+  }
 
-  private def getProducts(productIds: List[ProductId]): ZIO[ProductRepository, ProductError.NotFound, List[Product]] =
-    ZIO.foreach(productIds)(ProductRepository.getById)
+  def createOrder(orderDto: OrderDto): ZIO[
+    OrderRepository & CustomerRepository & ProductRepository,
+    Union3[ValidationFailure, CustomerError.NotFound, ProductError.NotFound],
+    OrderId
+  ] =
+    for {
+      productsAndDetailDTOs <- ZIO
+        .foreach(orderDto.orderDetails) { detailDto =>
+          val id = ProductId.fromUUID(detailDto.productId)
+          ProductRepository.getById(id).map(_ -> detailDto)
+        }
+        .mapError(Union3.Third.apply)
 
-  def createOrder(createOrder: CreateOrder): URIO[OrderRepository, OrderId] = ???
+      customer <- CustomerRepository.getById(CustomerId.fromUUID(orderDto.customerId)).mapError(Union3.Second.apply)
+      orderId = OrderId.generate
+      orderDetails = productsAndDetailDTOs.map { case (product, detailDto) =>
+        detailDto.toDomain(orderId, product)
+      }.toList
+      order <- ZIO.fromNotValidated(orderDto.toDomain(customer, orderDetails)).mapError(Union3.First.apply)
+      id    <- OrderRepository.create(order).mapError(Union3.Third.apply)
+    } yield id
 
   def updateStatus(orderId: OrderId, orderStatus: OrderStatus): ZIO[OrderRepository, OrderError, Unit] =
     OrderRepository.updateStatus(orderId, orderStatus)
