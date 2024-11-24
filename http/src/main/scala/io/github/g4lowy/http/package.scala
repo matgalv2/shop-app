959dd1c4e7563ca16a52449121be98e7a5ae6c78
mateusz <matgalv2@gmail.com>
2024-11-24 13:41:09 +0100

    Added type alias for Validator.FailureDescription

--- http/src/main/scala/io/github/g4lowy/http/package.scala
+++ http/src/main/scala/io/github/g4lowy/http/package.scala
@@ -1,10 +1,12 @@
 package io.github.g4lowy
 
-import io.github.g4lowy.http.api.{ CustomerApi, OrderApi, ProductApi }
+import io.github.g4lowy.http.api.{CustomerApi, OrderApi, ProductApi}
+import io.github.g4lowy.validation.validators.Validator
 import zio.&
 
 package object http {
 
   type AppEnvironment = CustomerApi.Environment & ProductApi.Environment & OrderApi.Environment
 
+  type ValidationFailure = Validator.FailureDescription
 }
