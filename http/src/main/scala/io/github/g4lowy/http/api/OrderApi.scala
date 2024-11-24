87a8edd54660c05f509c29e39962963b93f80848
mateusz <matgalv2@gmail.com>
2024-11-24 14:02:13 +0100

    Implemented api handler methods

--- http/src/main/scala/io/github/g4lowy/http/api/OrderApi.scala
+++ http/src/main/scala/io/github/g4lowy/http/api/OrderApi.scala
@@ -1,46 +1,79 @@
 package io.github.g4lowy.http.api
 
-import http.generated.definitions.{ CreateOrder, PatchOrder }
+import http.generated.definitions.{ CreateOrder, ErrorResponse, PatchOrder }
+import http.generated.orders
 import http.generated.orders.{
   CreateOrderResponse,
   GetAllOrdersResponse,
   GetOrderByIdResponse,
   OrdersHandler,
   OrdersResource,
-  PatchOrderResponse
+  UpdateStatusResponse
 }
+import io.github.g4lowy.customer.domain.repository.CustomerRepository
+import io.github.g4lowy.error.ErrorMessage._
 import io.github.g4lowy.http.AppEnvironment
 import io.github.g4lowy.http.api.OrderApi.Environment
+import io.github.g4lowy.http.converters.orders.{ CreateOrderOps, OrderIdOps, OrderOps, PatchOrderStatusOps }
+import io.github.g4lowy.http.error._
+import io.github.g4lowy.http.service.OrderService
+import io.github.g4lowy.order.domain.model.{ OrderError, OrderId }
 import io.github.g4lowy.order.domain.repository.OrderRepository
 import io.github.g4lowy.product.domain.repository.ProductRepository
+import io.github.g4lowy.union.types.Union3
 import org.http4s.HttpRoutes
 import zio.{ &, RIO, Runtime, ZIO }
 
 import java.util.UUID
 
 class OrderApi extends OrdersHandler[RIO[AppEnvironment, *]] {
-  override def createOrder(respond: CreateOrderResponse.type)(
-    body: CreateOrder
-  ): RIO[Environment, CreateOrderResponse] = ???
+  override def createOrder(
+    respond: CreateOrderResponse.type
+  )(body: CreateOrder): RIO[Environment, CreateOrderResponse] =
+    OrderService
+      .createOrder(body.toDTO)
+      .mapBoth(
+        {
+          case Union3.First(validationFailure) => respond.BadRequest(ErrorResponse.single(validationFailure.toMessage))
+          case Union3.Second(customerNotFound) => respond.BadRequest(ErrorResponse.single(customerNotFound.toMessage))
+          case Union3.Third(productsNotFound)  => respond.BadRequest(ErrorResponse.single(productsNotFound.toMessage))
+        },
+        orderId => respond.Created(orderId.toAPI)
+      )
+      .merge
 
   override def getAllOrders(
     respond: GetAllOrdersResponse.type
-  )(offset: Option[Int], limit: Option[Int]): RIO[Environment, GetAllOrdersResponse] = ???
-//    val offsetV = offset.getOrElse(DEFAULT_OFFSET)
-//    val limitV  = limit.getOrElse(DEFAULT_LIMIT)
-//
-//    OrderService.getOrders(offsetV, limitV).map(_.map(_.toAPI))
+  )(offset: Option[Int], limit: Option[Int]): RIO[Environment, GetAllOrdersResponse] =
+    OrderService
+      .getOrders(offset, limit)
+      .map(_.map(_.toAPI).toVector)
+      .map(respond.Ok)
 
   override def getOrderById(respond: GetOrderByIdResponse.type)(orderId: UUID): RIO[Environment, GetOrderByIdResponse] =
-    ???
+    OrderService
+      .getOrderById(orderId)
+      .map(_.toAPI)
+      .mapBoth(error => respond.NotFound(ErrorResponse.single(error.toMessage)), respond.Ok)
+      .merge
 
-  override def patchOrder(
-    respond: PatchOrderResponse.type
-  )(orderId: UUID, body: PatchOrder): RIO[Environment, PatchOrderResponse] = ???
+  override def updateStatus(
+    respond: orders.UpdateStatusResponse.type
+  )(orderId: UUID, body: PatchOrder): RIO[Environment, UpdateStatusResponse] =
+    OrderService
+      .updateStatus(OrderId.fromUUID(orderId), body.status.toDomain)
+      .mapBoth(
+        {
+          case err: OrderError.NotFound      => respond.NotFound(ErrorResponse.single(err.toMessage))
+          case err: OrderError.InvalidStatus => respond.BadRequest(ErrorResponse.single(err.toMessage))
+        },
+        _ => respond.NoContent
+      )
+      .merge
 }
 
 object OrderApi {
-  type Environment = OrderRepository & ProductRepository
+  type Environment = OrderRepository & ProductRepository & CustomerRepository
 
   val routes: RIO[AppEnvironment, HttpRoutes[RIO[AppEnvironment, *]]] = {
     import zio.interop.catz._
