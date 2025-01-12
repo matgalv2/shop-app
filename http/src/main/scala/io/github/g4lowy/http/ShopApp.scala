215886ea13c1688ee59af525381d0746cd19e01f
mateusz <matgalv2@gmail.com>
2025-01-12 18:05:35 +0100

    Updated todos list

--- http/src/main/scala/io/github/g4lowy/http/ShopApp.scala
+++ http/src/main/scala/io/github/g4lowy/http/ShopApp.scala
@@ -33,24 +33,12 @@ object ShopApp extends ZIOAppDefault {
   private val handleBackground = {
     for {
       _ <- OrderJobs.archiveOrdersOnceADay.forkDaemon
-//      _ <- ZIO.log(getMemoryStatistics).repeat(Schedule.fixed(5.minutes)).forkDaemon
     } yield ()
   }
 
-//  private def getMemoryStatistics = {
-//    val runtime = Runtime.getRuntime
-//
-//    val `2^30`: Double = 1024 * 1024 * 1024
-//
-//    s"Current RAM usage ${runtime.totalMemory() / `2^30`}GB / ${runtime.maxMemory() / `2^30`}GB"
-//  }
-
   /*
    TODO:
-    1. find a way to expose special services from one module to another
-    2. add order mapper tests
-    3. ----add "ARCHIVED" order status and make cyclic task that archives orders finished 3 months ago from now---
-    4. add tests ^^^
-    5. add triggers to database (update order status to move archived orders to different table)
+    1. add test for archiving method
+    2. OrderRepository utilizes ProductError - refactor
    */
 }
