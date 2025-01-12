28515fcb4189858cf9342097ad6b62ebe3925713
mateusz <matgalv2@gmail.com>
2025-01-12 15:04:39 +0100

    Changed required id to super type

--- http/src/main/scala/io/github/g4lowy/http/service/ProductService.scala
+++ http/src/main/scala/io/github/g4lowy/http/service/ProductService.scala
@@ -1,5 +1,6 @@
 package io.github.g4lowy.http.service
 
+import io.github.g4lowy.abstracttype.Id
 import io.github.g4lowy.product.domain.model.{Product, ProductError, ProductId}
 import io.github.g4lowy.product.domain.repository.ProductRepository
 import zio.{URIO, ZIO}
@@ -7,22 +8,22 @@ import zio.{URIO, ZIO}
 object ProductService {
 
   private val DEFAULT_OFFSET = 0
-  private val DEFAULT_LIMIT = 10
+  private val DEFAULT_LIMIT  = 10
 
   def getProducts(offset: Option[Int], limit: Option[Int]): URIO[ProductRepository, List[Product]] =
     ProductRepository.getAll(offset.getOrElse(DEFAULT_OFFSET), limit.getOrElse(DEFAULT_LIMIT))
 
-  def getMany(ids: List[ProductId]): ZIO[ProductRepository, ProductError.NotFound, List[Product]] =
-    ProductRepository.getMany(ids)
+  def getMany(ids: List[Id]): ZIO[ProductRepository, ProductError.NotFound, List[Product]] =
+    ProductRepository.getMany(ids.map(id => ProductId.fromUUID(id.value)))
 
-  def getProductById(productId: ProductId): ZIO[ProductRepository, ProductError.NotFound, Product] =
-    ProductRepository.getById(productId)
+  def getProductById(id: Id): ZIO[ProductRepository, ProductError.NotFound, Product] =
+    ProductRepository.getById(ProductId.fromUUID(id.value))
 
   def createProduct(product: Product): URIO[ProductRepository, ProductId] = ProductRepository.create(product)
 
-  def updateProduct(productId: ProductId, product: Product): ZIO[ProductRepository, ProductError.NotFound, Unit] =
-    ProductRepository.update(productId, product)
+  def updateProduct(id: Id, product: Product): ZIO[ProductRepository, ProductError.NotFound, Unit] =
+    ProductRepository.update(ProductId.fromUUID(id.value), product)
 
-  def deleteProduct(productId: ProductId): ZIO[ProductRepository, ProductError.NotFound, Unit] =
-    ProductRepository.delete(productId)
+  def deleteProduct(id: Id): ZIO[ProductRepository, ProductError.NotFound, Unit] =
+    ProductRepository.delete(ProductId.fromUUID(id.value))
 }
