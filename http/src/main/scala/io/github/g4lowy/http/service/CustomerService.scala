ab31eacf6acde84869c58171a650876eccddbed9
mateusz <matgalv2@gmail.com>
2025-01-12 15:04:44 +0100

    Changed required id to super type

--- http/src/main/scala/io/github/g4lowy/http/service/CustomerService.scala
+++ http/src/main/scala/io/github/g4lowy/http/service/CustomerService.scala
@@ -1,5 +1,6 @@
 package io.github.g4lowy.http.service
 
+import io.github.g4lowy.abstracttype.Id
 import io.github.g4lowy.customer.domain.model.{Customer, CustomerError, CustomerId}
 import io.github.g4lowy.customer.domain.repository.CustomerRepository
 import zio.{URIO, ZIO}
@@ -12,8 +13,8 @@ object CustomerService {
   def getCustomers(offset: Option[Int], limit: Option[Int]): URIO[CustomerRepository, List[Customer]] =
     CustomerRepository.getAll(offset.getOrElse(DEFAULT_OFFSET), limit.getOrElse(DEFAULT_LIMIT))
 
-  def getCustomerById(customerId: CustomerId): ZIO[CustomerRepository, CustomerError.NotFound, Customer] =
-    CustomerRepository.getById(customerId)
+  def getCustomerById(id: Id): ZIO[CustomerRepository, CustomerError.NotFound, Customer] =
+    CustomerRepository.getById(CustomerId.fromUUID(id.value))
 
   def createCustomer(customer: Customer): URIO[CustomerRepository, CustomerId] = CustomerRepository.create(customer)
 
@@ -23,9 +24,9 @@ object CustomerService {
   ): ZIO[CustomerRepository, CustomerError.NotFound, Unit] =
     CustomerRepository.update(customerId, customer)
 
-  def deleteCustomer(customerId: CustomerId): ZIO[CustomerRepository, CustomerError.NotFound, Unit] =
+  def deleteCustomer(id: Id): ZIO[CustomerRepository, CustomerError.NotFound, Unit] =
     /*
       TODO: after implementing orders ensure that client that is going to be deleted, doesn't have active orders
      */
-    CustomerRepository.delete(customerId)
+    CustomerRepository.delete(CustomerId.fromUUID(id.value))
 }
